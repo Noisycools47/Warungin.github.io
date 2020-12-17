353f61088d4284aeb98a06d06fc5f11fdbbf4903
mrz-f0Th <72551171+mrz-f0Th@users.noreply.github.com>
2020-12-17 08:33:17 -0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,35 @@
+// Awal Navbar
+const check = document.querySelector('.check');
+const navigasi = document.querySelector('.nav');
+const user = document.querySelector('.user');
+navigasi.style.display = 'none';
+
+check.addEventListener('click', ()=> {
+
+    if(navigasi.style.display === 'none'){
+        check.style.transform = 'rotate(90deg)';
+        navigasi.style.animation = 'animasi1 1s';
+        user.style.animation = 'animasi2 1s';
+        navigasi.style.display = 'flex';
+        user.style.display = 'flex';
+        navigasi.style.opacity = '1';
+        user.style.opacity = '1';
+        setTimeout(()=> {
+            navigasi.style.animation = '';
+            user.style.animation = '';
+        }, 1000);
+    }else if(navigasi.style.display === 'flex'){
+        check.style.transform = 'rotate(0deg)';
+        navigasi.style.animation = 'animasi1 .5s reverse';
+        user.style.animation = 'animasi2 .2s reverse';
+        navigasi.style.opacity = '0';
+        user.style.opacity = '0';
+        setTimeout(()=> {
+            user.style.display = 'none';
+            navigasi.style.display = 'none';
+        }, 500);
+        
+    }
+});
+
+// Akhir Navbar
